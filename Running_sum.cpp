c2ce70ec6f4cb1a6812161b3e5d21fa2d3017046
Annuha Tabassum Nowrin <annuhatabassumnowrin@gmail.com>
2025-08-10 16:05:22 +0600

    Add files via upload

---
+++ Running_sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter array size:";
+    cin >> n;
+    cout << "Enter array element:";
+    int arr[n];
+
+    for (int i = 0; i < n;i++)
+        cin >> arr[i];
+    int pre_sum[n];
+    pre_sum[0] = arr[0];
+
+    for (int i = 1; i < n;i++)
+        pre_sum[i] = pre_sum[i-1] + arr[i];
+    
+
+    cout << "Running sum: ";
+    for (int i = 0; i < n; i++)
+        cout << pre_sum[i] << " ";
+
+    return 0;
+}
